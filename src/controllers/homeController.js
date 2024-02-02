41d49fa5eb1eeef0a58525e4fc06a6586b8d3361
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2024-02-02 19:55:08 +0700

    edit, delete user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -27,14 +27,47 @@ let displayGetCRUD = async (req, res) => {
     let data = await CRUDService.getAllUsers();
     console.log(data);
     return res.render('displayCRUD.ejs', {
-        dataTable: data
+        dataTable: data,
     });
 };
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+        return res.render('editCRUD.ejs', {
+            user: userData,
+        });
+    } else {
+        return res.send('user not found');
+    }
+};
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers,
+    });
+};
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if(id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('delete user successfully');
+    }
+    return res.send('user not found');
+
+};
+
 module.exports = {
-    getHomePage: getHomePage,
-    getAboutPage: getAboutPage,
-    getCRUD: getCRUD,
-    postCRUD: postCRUD,
+    getHomePage,
+    getAboutPage,
+    getCRUD,
+    postCRUD,
     displayGetCRUD,
+    getEditCRUD,
+    putCRUD,
+    deleteCRUD,
 };
