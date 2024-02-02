a106776c1345a8d54afc3465dc35f9c350b0732f
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2024-02-02 10:52:31 +0700

    create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
-import db from '../models/index'
+import db from '../models/index';
+import  CRUDService  from '../services/CRUDService';
 let getHomePage = async (req, res) => {
     try {
         let data = await db.User.findAll();
@@ -7,13 +8,24 @@ let getHomePage = async (req, res) => {
     } catch (error) {
         console.log(error);
     }
-} 
+};
 
 let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
-}
+};
+
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+};
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    return res.send('post crud');
+};
 
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
-}
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+};
