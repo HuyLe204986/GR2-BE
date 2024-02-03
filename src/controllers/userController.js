3f2c8e4c648b2bdac8072f7c1d07e2b8f0e71f7d
huy.ltn204986 <huy.ltn204986@sis.hust.edu.vn>
2024-02-03 13:21:15 +0700

    viet api lget-all-users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,7 @@ import userService from '../services/userService';
 let handleLogin = async (req, res) => {
     let email = req.body.email;
     let password = req.body.password;
-    console.log(email, password);
+    // console.log(email, password);
     if (!email || !password) {
         return res.status(500).json({
             errCode: 1,
@@ -19,6 +19,24 @@ let handleLogin = async (req, res) => {
     });
 };
 
+let handleGetAllUsers = async (req, res) => {
+    let id = req.query.id; // ALL, id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+            users: [],
+        });
+    }
+    let users = await userService.getAllUsers(id);
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'OK',
+        users,
+    });
+};
+
 module.exports = {
     handleLogin,
+    handleGetAllUsers,
 };
